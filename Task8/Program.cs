d878f7ead40239fc4f1ea3e765ea8d717a2a85c0
Docvel <docvel@yandex.ru>
2022-12-01 04:20:22 +0300

    solution of task 8, using loop for & TryParse

---
+++ Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите любое число больше 0: ");
+int even = 2;
+bool res = int.TryParse(Console.ReadLine(), out int num);
+if (res == true && num > 0) {
+    if (num % 2 == 0) {
+        Console.Write($"Чётные числа в диапазоне от 1 до {num}: ");
+        for (int i = 1; i <= num / 2; i++) {
+            Console.Write($"{even} ");
+            even = even + 2;}}
+    else {
+        Console.Write($"Чётные числа в диапазоне от 1 до {num}: ");
+        num = num - 1;
+        for (int i = 1; i <= num / 2; i++) {
+        Console.Write($"{even} ");
+        even = even + 2;}}
+}
+if (res == true && num <= 0) Console.Write("Введённое число не соответствует условиям задачи");
+if (res == false) Console.Write("Вы ввели не число");
