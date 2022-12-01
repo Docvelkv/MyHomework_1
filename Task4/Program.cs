02301972bd039d7df7e23bbe619f624902f48248
Docvel <docvel@yandex.ru>
2022-12-01 04:07:54 +0300

    solution of task 4, using array

---
+++ Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел
+// Решено через создание массива.
+Console.Clear();
+Console.Write("Введите количество элементов массива: "); // Количество элементов массива
+int n = int.Parse(Console.ReadLine());
+int[] ar = new int[n]; // создание пустого массива
+int i = 0;
+Console.Clear();
+// Заполнение массива, иначе через цикл for (i = 0; i < ar.Lenght; i++){...}
+while ( i < ar.Length ) {                               
+    Console.Write($"Введите элемент массива под индексом {i}: ");
+    ar[i] = int.Parse(Console.ReadLine());
+    Console.Clear();
+    i++;                                                            
+}
+// определение максимального значения и вывод результатов, иначе через цикл for
+i = 0;
+int index = 0;
+int max = ar[0];
+Console.Write("Массив [ ");
+while ( i < ar.Length) {
+    if ( max < ar[i]) {
+        max = ar[i];
+        index = i;
+    }
+    Console.Write($"{ar[i]} ");
+    i++;    
+}
+Console.Write("]. ");
+Console.Write($"Максимальное число в массиве равно {max}, значение № - {index + 1}.");
