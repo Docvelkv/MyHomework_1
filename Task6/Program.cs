972139c24413bc4c9af90d93b301f41e1deba9f5
Docvel <docvel@yandex.ru>
2022-12-01 04:12:47 +0300

    solution of task 6, using TryParse

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и
+// выдаёт, является ли число чётным
+// Решение с помощью метода TryParse
+Console.Clear();
+Console.Write("Введите число: ");
+bool res = int.TryParse(Console.ReadLine(), out int num);
+if ( res == true) {
+    if ( num % 2 == 0) Console.Write($"Вы ввели число {num} оно является чётным");
+    else Console.Write($"Вы ввели число {num} оно не является чётным");}
+else Console.Write("Вы ввели не число");
